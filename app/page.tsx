a0e8d76ed98810ba94e2ae13aaa2a4a6be28d48f
biyou <3071709373@qq.com>
2024-04-17 11:09:18 +0800

    over

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,10 @@ import { F } from "@upstash/redis/zmscore-5d82e632";
 const subdomainToLabel = {
   [subdomains.CONSTANTS]: "Constants",
   [subdomains.EPOCH_CONVERTER]: "Epoch Converter",
-  [subdomains.EXPLORER]: "Explorer",
   [subdomains.CONVERTER]: "Converter",
   [subdomains.TRANSACT]: "Transact",
   [subdomains.CALLDATA]: "Calldata",
   [subdomains.STORAGE_SLOTS]: "Storage Slots",
-  [subdomains.UNISWAP]: "Uniswap V3",
   [subdomains.CHARACTER_COUNT]: "Character Counter",
   [subdomains.SIGNATURE]: "Signature",
   [subdomains.ACCOUNTCREATE]: "AccountCreate",
--- subdomains.js
+++ subdomains.js
@@ -1,12 +1,10 @@
 const subdomains = {
   CONSTANTS: "constants",
   EPOCH_CONVERTER: "epoch-converter",
-  EXPLORER: "explorer",
   CONVERTER: "converter",
   TRANSACT: "transact",
   CALLDATA: "calldata",
   STORAGE_SLOTS: "storage-slots",
-  UNISWAP: "uniswap",
   CHARACTER_COUNT: "character-counter",
   SIGNATURE: "signature",
   ACCOUNTCREATE: "accountcreate",
